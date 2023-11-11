0fd3a7544a4bf3f5ac7e297b3d8c0f58fd21aba3
tamani phiri <tamanigabriel0@gmail.com>
2023-11-11 18:29:52 +0200

    c

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,11 @@
 import { prisma } from "../config/db";
 
+export interface IUser {
+  id?: number;
+  name: string;
+  email: string;
+  password: string;
+  phone?: string;
+}
+
 export const User = prisma.user;
---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,17 @@
+import { IUser, User } from "../models/User";
+
+const userRepository = () => {
+  const createUser = async ({ name, email, password, phone }: IUser) => {
+    await User.create({
+      data: {
+        name,
+        email,
+        password,
+        phone,
+      },
+    });
+  };
+  return {
+    createUser,
+  };
+};
