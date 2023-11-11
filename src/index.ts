0f9ab75df2fa64596ea79d99d5643e8914e82fa1
tamani phiri <tamanigabriel0@gmail.com>
2023-11-11 18:13:16 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import express, { Express } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
 
 const app: Express = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(process.env.PORT, () => {
+  console.log(`server on @ http://localhost:${process.env.PORT}`);
+});
