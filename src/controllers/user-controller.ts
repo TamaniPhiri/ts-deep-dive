efcb0042a493a752997db102776048921e21dcde
tamani phiri <tamanigabriel0@gmail.com>
2023-11-11 20:04:45 +0200

    c

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,21 +1,23 @@
 import userService from "../services/user-service";
 import { Response, Request } from "express";
 import HttpStatusCode from "http-status-codes";
+import { User } from "../models/User";
 
 const userController = {
   userRegister: async (req: Request, res: Response) => {
     const { name, email, password } = req.body;
     try {
-      await userService
-        .GetUserByEmailAndPassword({
+      const existingUser = await User.findFirst({
+        where: {
           email,
           password,
-        })
-        .then(() => {
-          return res.status(HttpStatusCode.UNAUTHORIZED).send({
-            error: "User already exists",
-          });
+        },
+      });
+      if (existingUser) {
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({
+          error: "User already exists",
         });
+      }
       const user = await userService.CreateUser({ name, email, password });
       return res.json(user);
     } catch (error) {
