28ea04ff8b307c02215887731fbe9148da09c37b
tamani phiri <tamanigabriel0@gmail.com>
2023-11-11 22:55:07 +0200

    changes

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import userService from "../services/user-service";
 import { Response, Request } from "express";
 import HttpStatusCode from "http-status-codes";
@@ -19,7 +20,7 @@ const userController = {
         });
       }
 
-      const hashedPassword = await bcrypt.hash(password, 20);
+      const hashedPassword = await bcrypt.hash(password, 25);
 
       const user = await userService.CreateUser({
         name,
@@ -46,6 +47,18 @@ const userController = {
           error: "User doesn't exist",
         });
       }
+      const verifyPassword = await bcrypt.compare(user.password, password);
+      if (!verifyPassword) {
+        return res.status(HttpStatusCode.UNAUTHORIZED).send({
+          error: "Invalid Password ",
+        });
+      }
+      const token = await jwt.sign(user, process.env.JWT_SECRET as string, {
+        expiresIn: "3s",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
     } catch (error) {
       console.log(error);
       res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({
