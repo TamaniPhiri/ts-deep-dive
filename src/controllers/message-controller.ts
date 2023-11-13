33af4770ec736ef857d096ddb44fef68b0785836
tamani phiri <tamanigabriel0@gmail.com>
2023-11-13 15:27:01 +0200

    success

--- src/controllers/message-controller.ts
+++ src/controllers/message-controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
 import StatueCodes from "http-status-codes";
+import messageService from "../services/message-service";
 
 const MessageController = () => {
   const CreateMessage = async (req: Request, res: Response) => {
     try {
-      const { senderId, receiverid } = req.params;
+      const senderId = parseInt(req.params.senderId);
+      const receiverId = parseInt(req.params.receiverId);
       const { content } = req.body;
+      const message = await messageService.SendMessage(
+        senderId,
+        receiverId,
+        content
+      );
     } catch (error) {
       console.log(error);
       res.status(StatueCodes.INTERNAL_SERVER_ERROR).send({
