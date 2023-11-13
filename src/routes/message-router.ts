e06fcc9cedc5dbd81d88b7e0655f160494764d2c
tamani phiri <tamanigabriel0@gmail.com>
2023-11-13 15:48:46 +0200

    success

--- src/routes/message-router.ts
+++ src/routes/message-router.ts
@@ -5,7 +5,7 @@ import Auth from "../middleware/Auth";
 const messageRouter: Router = Router();
 
 messageRouter.post(
-  "/message/:senderId/:receiverId",
+  "/:senderId/:receiverId",
   Auth,
   messageController.CreateMessage
 );
