c49e6a835afa81f8f5d502c30f4538a3239ee43b
tamani phiri <tamanigabriel0@gmail.com>
2023-11-13 15:14:59 +0200

    success

---
+++ src/controllers/message-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import StatueCodes from "http-status-codes";
+
+const MessageController = () => {
+  const CreateMessage = async (req: Request, res: Response) => {
+    try {
+      const { senderId, receiverid } = req.params;
+      const { content } = req.body;
+    } catch (error) {
+      console.log(error);
+      res.status(StatueCodes.INTERNAL_SERVER_ERROR).send({
+        error: error,
+      });
+    }
+  };
+  return { CreateMessage };
+};
+
+export default MessageController();
