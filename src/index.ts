b71160ba4e35a3bc9ae003cfad2e00889ec330ef
tamani phiri <tamanigabriel0@gmail.com>
2023-11-13 15:48:26 +0200

    success

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,17 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import userRouter from "./routes/user-route";
+import messageRouter from "./routes/message-router";
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRouter);
+app.use("/message", messageRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`server on @ http://localhost:${process.env.PORT}`);
