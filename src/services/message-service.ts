7f2271070755f5c29c5b53b4bdb1e19153c67cb2
tamani phiri <tamanigabriel0@gmail.com>
2023-11-13 12:04:21 +0200

    success

---
+++ src/services/message-service.ts
@@ -0,0 +1,19 @@
+import messageRepo from "../repositories/message-repo";
+
+const MessageService = () => {
+  const SendMessage = async (
+    senderId: number,
+    receiverId: number,
+    content: string
+  ) => {
+    const message = await messageRepo.sendMessage(
+      senderId,
+      receiverId,
+      content
+    );
+    return message;
+  };
+  return {
+    SendMessage
+  };
+};
