b5b621d61a0501faeddb85c08134f63d6f476e0f
tamani phiri <tamanigabriel0@gmail.com>
2023-11-13 11:19:04 +0200

    changes

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/user-controller";
 const userRouter: Router = Router();
 
 userRouter.post("/register",userController.userRegister);
+userRouter.post("/login",userController.userLogin);
 
 export default userRouter;
